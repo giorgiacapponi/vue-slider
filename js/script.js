3f3fdb83f9f9f8503b379dca97b4e273f7912c6c
giorgiacapponi <giorgiacapponi3@gmail.com>
2023-03-29 21:13:27 +0200

    1-2

---
+++ js/script.js
@@ -0,0 +1,73 @@
+
+
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            slides:[
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                },
+                
+            ],
+           
+           imgVisible:0,
+           
+      
+
+        }
+        
+    },
+    mounted(){
+        setInterval(this.nextPhoto,3000)
+       
+
+    },
+    methods: {
+     nextPhoto(){
+        if(this.imgVisible<this.slides.length -1){
+
+          this.imgVisible++;
+        }else{
+           this.imgVisible=0;
+        }
+     },
+     prevPhoto(){
+        if(this.imgVisible>0){
+            this.imgVisible--;
+        }else{
+            this.imgVisible= this.slides.length -1;
+        }
+     },
+     showImgBig(index){
+     this.imgVisible=index
+     
+     },
+
+ 
+    
+    
+    
+     
+       
+    }
+
+}).mount('#app')
